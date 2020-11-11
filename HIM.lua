f6d617c26606a40fffcd488673455c5889cf5816
Hammi <alex.hammans@gmail.com>
2020-11-11 22:13:19 +0100

    Restructure, optimazation and additional functionality

--- HIM.lua
+++ HIM.lua
@@ -1,55 +1,61 @@
+--General Settings
 lengthOfRows = 10
 numberOfRows = 5
 maxTorchDistance = 10
-useTorches = false
-torchPlacement = 0
-useChest = false
-remoteStorage = false
-remoteStorageName = {"enderstorage:ender_storage"}
-storageName = ""
+runningLowWarning = 16
 
-function forward(times)
-  times = times or 1
-  for i=1,times do
-    while (not turtle.forward()) do
-      turtle.dig()
-      sleep(0.4)
+-- User Input Settings
+useTorches = false
+useChest = false
+clearInv = false
+waitForTorches = false
+allowMultipleStacks = false
+
+-- Work Settings (DONT CHANGE...Changed by Program)
+torchPlacement = 0
+remoteStorage = false
+remoteStorageNames = {"enderstorage:ender_storage"}
+storageName = ""
+firstOpenSlot = 2
+
+function has_value (tab, val)
+    for index, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
     end
-    while (turtle.detectUp()) do
-      b, k = turtle.inspectUp()
-      if b == true and k.name ~= "minecraft:torch" then
-        turtle.digUp()
-        sleep(0.4)
-      else
-        break
+    return false
+end
+
+function itemInSlot(slot,name)
+  details = turtle.getItemDetail(slot)
+  if details ~= nil and details.name == name then
+      return true
+  end
+  return false
+end
+
+function itemInInventory (name,deadSlot)
+  local outputarray = {}
+  for i=1,16 do
+    if i ~= deadSlot then
+      if itemInSlot(i,name) then
+        outputarray[#outputarray+1]= i
       end
     end
-    if turtle.getItemCount(1) < 16 then
-      sortItems("minecraft:cobblestone",1)
-    end
-    turtle.select(1)
-    turtle.placeDown()
-    checkInventoryFull(16)
-    placeTorch()
   end
+  return outputarray
 end
 
-function nextRow()
-  forward(3)
+function missingItem (name)
+  return (next(itemInInventory(name)) == nil) and true or false
 end
-----------------
-function sortItems(name, slot)
-  waitForItem(name)
-  details = turtle.getItemDetail(slot)
-  if details ~= nil and details.name ~= name then
-    freeUpSlot(slot)
-  end
-  for i,v in ipairs(checkForItemInInventory(name,slot)) do
-    turtle.select(v)
-    turtle.transferTo(slot)
-    if turtle.getItemCount(slot)>=63 then
-      turtle.select(1)
-      break
+
+function waitForItem (name)
+  if missingItem(name)  then
+    print("Please insert: "..name)
+    while missingItem(name) do
+      sleep(0.5)
     end
   end
 end
@@ -72,19 +78,32 @@ function freeUpSlot(slot) -- can return slot number
   return slot
 end
 
-function checkForItemInInventory (name,deadSlot)
-  local outputarray = {}
-  for i=1,16 do
-    if i ~= deadSlot then
-      details = turtle.getItemDetail(i)
-      if details ~= nil then
-        if details.name == name then
-          outputarray[#outputarray+1]= i
-        end
-      end
+function sortItems(slot,name)
+  waitForItem(name)
+  if not itemInSlot(slot,name) then
+    freeUpSlot(slot)
+  end
+  for i,v in ipairs(itemInInventory(name,slot)) do
+    turtle.select(v)
+    turtle.transferTo(slot)
+    if turtle.getItemCount(slot)>=64 then
+      turtle.select(1)
+      break
     end
   end
-  return outputarray
+end
+
+function __checkForStorageName()
+  if not missingItem("minecraft:chest") then
+    return "minecraft:chest"
+  end
+  for k,v in pairs(remoteStorageNames) do
+    if not missingItem(v) then
+      remoteStorage = true
+      return v
+    end
+  end
+  return ""
 end
 
 function checkForStorageName()
@@ -100,92 +119,112 @@ function checkForStorageName()
   return storageName
 end
 
-function __checkForStorageName()
-  if not missingItem("minecraft:chest") then
-    return "minecraft:chest"
-  end
-  for k,v in pairs(remoteStorageName) do
-    if not missingItem(v) then
-      remoteStorage = true
-      return v
-    end
-  end
-  return ""
-end
-
-function placeTorch()
-  if torchPlacement>=10 then
-      if turtle.getItemCount(2)>0 then
-        if turtle.getItemDetail(2).name == "minecraft:torch" then
-          turtle.turnLeft()
-          turtle.turnLeft()
-          turtle.select(2)
-          while not turtle.place() do
-            turtle.dig()
-            sleep(0.5)
-          end
-          turtle.turnLeft()
-          turtle.turnLeft()
+function dropInventory (direction)
+  for i=firstOpenSlot,16 do
+    if (allowMultipleStacks and not (itemInSlot(i,storageName) or itemInSlot(i,"minecraft:torches"))) or not allowMultipleStacks then
+      turtle.select(i)
+      if direction == "up" then
+        turtle.dropUp()
+      else
+        turtle.dropDown()
+      end
+      while turtle.getItemCount(i)>0 do
+        print("Couldn't drop items... trying again")
+        sleep(2)
+        turtle.select(i)
+        if direction == "up" then
+          turtle.dropUp()
+        elseif direction == "down" then
+          turtle.dropDown()
         else
-          print("Out of torches!")
+          error("Unknown direction to drop to")
         end
       end
-      turtle.select(1)
-      torchPlacement = 0
-  end
-  torchPlacement = torchPlacement + 1
+    end
 end
 
-function missingItem (name)
-  return (next(checkForItemInInventory(name)) == nil) and true or false
+function clearInventory ()
+  sortItems(chestSlot,storageName)
+  if remoteStorage then
+    turtle.select(chestSlot)
+    turtle.placeUp()
+    dropInventory("up")
+    turtle.select(chestSlot)
+    turtle.digUp()
+    end
+  else
+    turtle.digDown()
+    turtle.down()
+    turtle.select(1)
+    turtle.placeDown()
+    turtle.up()
+    turtle.select(chestSlot)
+    turtle.placeDown()
+    dropInventory("down")
+  end
+  turtle.select(1)
 end
 
 function checkInventoryFull(slot)
   if useChest and turtle.getItemCount(slot)>0 then
-    if not missingItem(storageName) then --sortItems(chest, 3)
-      if remoteStorage then
-        turtle.select(3)
-        turtle.placeUp()
-        for i=4,16 do
-          turtle.select(i)
-          turtle.dropUp()
-          while turtle.getItemCount(i)>0 do
-            print("Couldn't drop items... trying again")
-            sleep(2)
-            turtle.select(3)
-            turtle.digUp()
-          end
-        end
-      else
-        turtle.digDown()
-        turtle.down()
-        turtle.select(1)
-        turtle.placeDown()
-        turtle.up()
-        turtle.select(3)
-        turtle.placeDown()
-        for i=4,16 do
-          turtle.select(i)
-          turtle.dropDown()
-        end
+    clearInventory()
+  end
+end
+
+function placeTorch()
+  if torchPlacement>=10 then
+    if itemInSlot(torchSlot,"minecraft:torch") then
+      turtle.turnLeft()
+      turtle.turnLeft()
+      turtle.select(torchSlot)
+      while not turtle.place() do
+        turtle.dig()
+        sleep(0.5)
       end
+      turtle.turnLeft()
+      turtle.turnLeft()
+      turtle.select(1)
+      if turtle.getItemCount(torchSlot) <= runningLowWarning then
+        print("Running low on torches")
+      end
+      torchPlacement = 0
     else
-      sortItems(storageName,3)
+      if waitForTorches then
+        sortItems(torchSlot,"minecraft:torch")
+      else
+        print("Out of torches!")
+      end
+    end
+  end
+  torchPlacement = torchPlacement + 1
+end
+
+function forward(times)
+  times = times or 1
+  for i=1,times do
+    while (not turtle.forward()) do
+      turtle.dig()
+      sleep(0.4)
+    end
+    while (turtle.detectUp()) do
+      turtle.digUp()
+      sleep(0.4)
+    end
+    if turtle.getItemCount(1) < 16 then
+      sortItems(1,"minecraft:cobblestone")
     end
     turtle.select(1)
-  end
-end
-
-function waitForItem (name)
-  if missingItem(name)  then
-    print("Please insert: "..name)
-    while missingItem(name) do
-      sleep(0.5)
+    turtle.placeDown()
+    if useChest then
+      checkInventoryFull()
+    end
+    if useTorches then
+      placeTorch()
     end
   end
 end
 
---User Inputs--
+-- User Inputs ------------------------------------
 term.clear()
 term.setCursorPos(1,1)
 
@@ -193,16 +232,25 @@ local termInput = ""
 
 print("Start new Mine? (y for yes):")
 newMine = read()=="y" and true or false
-print("Length of rows? (Default: 10):")
+print("Length of rows? (Default: "..String(lengthOfRows).."):")
 termInput = read()
 lengthOfRows = termInput=="" and lengthOfRows or tonumber(termInput)
 print("Number of rows? (Default: 5): ")
 termInput = read()
 numberOfRows =  termInput=="" and numberOfRows or tonumber(termInput)
-print("Use Torches? (y for yes):")
+print("Following y for yes or anything else for no:")
+print("Use Torches?:")
 useTorches = read()=="y" and true
-print("Use Chest? (y for yes):")
+if useTorches then
+  print("Wait for Torches?:")
+  waitForTorches = read()=="y" and true
+end
+print("Use Chest?:")
 useChest = read()=="y" and true
+print("Clear Inventory at the end?:")
+clearInv = read()=="y" and true
+print("Allow MultipleStacks?:")
+allowMultipleStacks = read()=="y" and true
 
 term.clear()
 term.setCursorPos(1,1)
@@ -211,7 +259,10 @@ print("New mine: "..tostring(newMine))
 print("Row Length: "..tostring(lengthOfRows))
 print("Number of rows: "..tostring(numberOfRows))
 print("useTorches: "..tostring(useTorches))
+print("waitForTorches: "..tostring(waitForTorches))
 print("useChest: "..tostring(useChest))
+print("clearInv: "..tostring(clearInv))
+print("allowMultipleStacks: "..tostring(allowMultipleStacks))
 write("Order of Items in Inventory does not matter. \nPlease add: 1x64 Cobblestone ")
 if useTorches then
   write(", 1x64 Torches")
@@ -219,27 +270,32 @@ end
 if useChest then
     write(", 1x64 Chest or 1x Remote Storage")
 end
+if allowMultipleStacks then
+  write(". \nYou can also add more of Chests or Torches")
+end
 print("\nStart? (else CTRL+R)")
 read()
 
 term.clear()
 term.setCursorPos(1,1)
 
-sortItems("minecraft:cobblestone",1)
+sortItems(1,"minecraft:cobblestone")
 
 if useTorches then
-  sortItems("minecraft:torch",2)
+  torchSlot = firstOpenSlot
+  firstOpenSlot = firstOpenSlot + 1
+  sortItems(torchSlot,"minecraft:torch")
 end
 if useChest then
-  -- checkForStorageName()
-  sortItems(checkForStorageName(),3)
+  chestSlot = firstOpenSlot
+  firstOpenSlot = firstOpenSlot + 1
+  sortItems(chestSlot,checkForStorageName())
 end
 
 -- Main Programm --
-
 turtle.select(1)
 if(newMine) then
-  nextRow()
+  forward(3)
 end
 for i = 1,tonumber(numberOfRows) do
   print("starting row: "..tostring(i) .. " " .. turtle.getFuelLevel())
@@ -248,7 +304,7 @@ for i = 1,tonumber(numberOfRows) do
   forward(lengthOfRows)
   turtle.turnRight()
   -- Turnaround
-  nextRow()
+  forward(3)
   turtle.turnRight()
   forward(lengthOfRows)
   turtle.turnRight()
@@ -261,7 +317,7 @@ for i = 1,tonumber(numberOfRows) do
   forward(lengthOfRows)
   turtle.turnLeft()
   -- Turnaround
-  nextRow()
+  forward(3)
   turtle.turnLeft()
   forward(lengthOfRows)
   turtle.turnLeft()
@@ -269,10 +325,9 @@ for i = 1,tonumber(numberOfRows) do
   turtle.turnRight()
   turtle.turnRight()
 end
-print("Cleaning inventory!")
-for i=3,16 do
-  if turtle.getItemCount(i) > 1 and i > 5 then
-    checkInventoryFull(i)
-  end
+if clearInv then
+  print("Cleaning inventory!")
+  clearInventory()
 end
+
 print("Finished Mine")
